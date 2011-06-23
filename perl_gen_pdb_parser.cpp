c35dc02748411532b3bae98c3c9823975ef2103a
q10 <q101100111000@gmail.com>
2011-06-23 02:55:24 -0700

    testing pdb input

---
+++ perl_gen_pdb_parser.cpp
@@ -0,0 +1,52 @@
+#include "common.h"
+
+Chain * get_chain_from_perl_gen_pdb_file(string & filename) {
+    ifstream filestream(filename.c_str());
+    ASSERT(filestream.is_open(), "Could not open input PDB file " + STRING(filename));
+
+    Chain * chain = NULL;
+    Residue * current_residue = NULL;
+    Atom * current_atom = NULL;
+
+    string line, line_key, atom_name, resname, chain_id, element;
+    int atom_id, residue_id;
+    double * coords = new double [3];
+
+    while (getline(filestream, line)) {
+        element = line[13];
+        istringstream iss(line);
+        iss >> line_key; // get ATOM
+        iss >> atom_id; // get atom id
+        iss >> atom_name; // get atom name (ex CA)
+        iss >> resname; // get residue name (ex HIS)
+        iss >> chain_id; // get chain id
+        iss >> residue_id; // get residue id
+        iss >> coords[0];
+        iss >> coords[1];
+        iss >> coords[2];
+
+        if (chain == NULL)
+            chain = new Chain(chain_id);
+
+        if (current_residue == NULL or current_residue->id != residue_id) {
+            current_residue = new Residue(residue_id, resname);
+            chain->add_residue(current_residue);
+        }
+
+        current_atom = new Atom(atom_id, atom_name, coords, element);
+        current_residue->add_atom(current_atom);
+    }
+
+    return chain;
+}
+
+void test_get_chain_from_perl_gen_pdb_file() {
+    string filename = "sample.pdb";
+    Chain * chain = get_chain_from_perl_gen_pdb_file(filename);
+    cout << chain << endl;
+    Residue * residue = (*chain->residues)[0];
+    cout << residue << endl;
+    Atom * atom = (*residue->atoms)[0];
+    cout << atom << atom->coords[0] << " " << atom->coords[1] << " " << atom->coords[2] << endl;
+    return;
+}
